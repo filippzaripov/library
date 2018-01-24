6241c932ad54c465d1645ac22c63a71cf3a1fae4
filipp <philippsbucket@gmail.com>
2018-01-24 14:08:05 +0300

    Added javadoc to UserDAO interface

--- library-dao/src/main/java/com/fujitsu/internship/dao/UserDao.java
+++ library-dao/src/main/java/com/fujitsu/internship/dao/UserDao.java
@@ -8,16 +8,28 @@ import java.util.List;
  * interface to manipulate with users
  *
  * @see com.fujitsu.internship.dao.GenericDAO
+ * Each method throws DataAccessException
  */
-public interface UserDao extends GenericDAO<User>{
+public interface UserDao extends GenericDAO<User> {
     @Override
-    User create(User entity);
+    User create(User entity) throws DataAccessException;
 
     @Override
-    User get(Long id);
+    User get(Long id) throws DataAccessException;
 
     @Override
-    boolean delete(long id);
+    boolean delete(long id) throws DataAccessException;
 
-    List<User> getAll();
+    /**
+     * @return all users from storage
+     */
+    List<User> getAll() throws DataAccessException;
+
+    /**
+     * finds user in storage by name
+     *
+     * @param name name of user that should be found
+     * @return user object {@code null} if wasn't found
+     */
+    User findByName(String name) throws DataAccessException;
 }
